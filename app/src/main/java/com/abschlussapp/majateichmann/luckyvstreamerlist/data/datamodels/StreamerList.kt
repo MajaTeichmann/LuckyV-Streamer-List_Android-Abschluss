e700c89750d1a9e837437459cd4946f80d3cacaa
MajaTeichmann <katharina.nickname@hotmail.de>
2023-05-10 09:10:27 +0200

    10.05.2023 -> 09:09 Uhr
    -> StreamerList.kt "val online: Long," hinzugefügt

--- app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/data/datamodels/StreamerList.kt
+++ app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/data/datamodels/StreamerList.kt
@@ -2,5 +2,6 @@ package com.abschlussapp.majateichmann.luckyvstreamerlist.data.datamodels
 
 // diese Datenklasse beinhaltet eine Liste an Daten aus einer  weiteren Datenklasse ("Streamer")
 data class StreamerList(
+    val online: Long,
     val streamer: List<Streamer>
 )
