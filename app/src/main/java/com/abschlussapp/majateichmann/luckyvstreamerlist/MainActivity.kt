af840d1128c9de7a811ceff4c30e0853564a2f26
MajaTeichmann <katharina.nickname@hotmail.de>
2023-05-15 15:53:52 +0200

    15.05.2023 -> 15:53 Uhr

--- app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/MainActivity.kt
+++ app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.abschlussapp.majateichmann.luckyvstreamerlist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -8,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.abschlussapp.majateichmann.luckyvstreamerlist.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
+import com.abschlussapp.majateichmann.luckyvstreamerlist.others.data.TAG
 
 
 /**
@@ -15,98 +17,11 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     //damit Kopfbinde im StartFragemnt nicht zu sehen ist
     private lateinit var mainLayout: ConstraintLayout
 
-    private lateinit var navController: NavController
-
-    fun showMainLayout() {
-        binding.clAppHeader.visibility = View.VISIBLE
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val btnHome = binding.btnHome
-        val btnFavorites = binding.btnFavorites
-        val btnSettings = binding.btnSettings
-        val currentDestination = navController.currentDestination?.id
-
-        when (currentDestination) {
-            R.id.homeFragment -> {
-                btnHome.isEnabled = false
-                btnFavorites.isEnabled = true
-                btnSettings.isEnabled = true
-                btnFavorites.setOnClickListener {
-                    navController.navigate(R.id.action_homeFragment_to_favoritesFragment)
-                }
-
-                btnSettings.setOnClickListener {
-                    navController.navigate(R.id.action_homeFragment_to_settingsFragment)
-                }
-            }
-
-            R.id.favoritesFragment -> {
-                btnHome.isEnabled = true
-                btnFavorites.isEnabled = false
-                btnSettings.isEnabled = true
-                btnHome.setOnClickListener {
-                    navController.navigate(R.id.action_favoritesFragment_to_homeFragment)
-                }
-
-                btnSettings.setOnClickListener {
-                    navController.navigate(R.id.action_favoritesFragment_to_settingsFragment)
-                }
-            }
-
-            R.id.settingsFragment -> {
-                btnHome.isEnabled = true
-                btnFavorites.isEnabled = true
-                btnSettings.isEnabled = false
-                btnHome.setOnClickListener {
-                    navController.navigate(R.id.action_settingsFragment_to_homeFragment)
-                }
-
-                btnFavorites.setOnClickListener {
-                    navController.navigate(R.id.action_settingsFragment_to_favoritesFragment)
-                }
-            }
-        }
-
-        mainLayout = binding.clAppHeader
-
-        // Zeige das ConstraintLayout standardmäßig an
-        binding.clAppHeader.visibility = View.VISIBLE
-    }
-
-    fun ausblenden() {
-        binding.tvHeader.visibility = View.INVISIBLE
-        binding.ivLuckyvLogo.alpha = 0F
-
-        binding.clNavBar.visibility = View.INVISIBLE
-        binding.btnHome.visibility = View.INVISIBLE
-        binding.btnFavorites.visibility = View.INVISIBLE
-        binding.btnSettings.visibility = View.INVISIBLE
-    }
-
-    fun einblenden() {
-        binding.tvHeader.visibility = View.VISIBLE
-        binding.ivLuckyvLogo.alpha = 1F
-
-        binding.clNavBar.visibility = View.VISIBLE
-        binding.btnHome.visibility = View.VISIBLE
-        binding.btnFavorites.visibility = View.VISIBLE
-        binding.btnSettings.visibility = View.VISIBLE
-    }
-
-    //Wenn HomeFragment & StreamFragment sichtbar
     fun changeHomeColor() {
         val btnHome: ImageButton = findViewById(R.id.btn_home)
         btnHome.setImageResource(R.drawable.baseline_home_black_20)
@@ -140,5 +55,109 @@ class MainActivity : AppCompatActivity() {
 
         val btnSettings: ImageButton = findViewById(R.id.btn_settings)
         btnSettings.setImageResource(R.drawable.baseline_settings_black_20)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val btnHome = binding.btnHome
+        val btnFavorites = binding.btnFavorites
+        val btnSettings = binding.btnSettings
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    btnHome.isEnabled = false
+                    btnFavorites.isEnabled = true
+                    btnSettings.isEnabled = true
+                    btnFavorites.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_homeFragment_to_favoritesFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to FavoritesFragment failed: $e")
+                        }
+                    }
+
+                    btnSettings.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_homeFragment_to_settingsFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to SettingsFragment failed: $e")
+                        }
+                    }
+                }
+
+                R.id.favoritesFragment -> {
+                    btnHome.isEnabled = true
+                    btnFavorites.isEnabled = false
+                    btnSettings.isEnabled = true
+                    btnHome.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_favoritesFragment_to_homeFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to HomeFragment failed: $e")
+                        }
+                    }
+
+                    btnSettings.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_favoritesFragment_to_settingsFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to SettingsFragment failed: $e")
+                        }
+                    }
+                }
+
+                R.id.settingsFragment -> {
+                    btnHome.isEnabled = true
+                    btnFavorites.isEnabled = true
+                    btnSettings.isEnabled = false
+                    btnHome.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_settingsFragment_to_homeFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to HomeFragment failed: $e")
+                        }
+                    }
+
+                    btnFavorites.setOnClickListener {
+                        try {
+                            navController.navigate(R.id.action_settingsFragment_to_favoritesFragment)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Navigation to FavoritesFragment failed: $e")
+                        }
+                    }
+                }
+            }
+        }
+        // Zeige das ConstraintLayout standardmäßig an
+        binding.clAppHeader.visibility = View.VISIBLE
+    }
+
+
+    fun ausblenden() {
+        binding.tvHeader.visibility = View.INVISIBLE
+        binding.ivLuckyvLogo.alpha = 0F
+
+        binding.clNavBar.visibility = View.INVISIBLE
+        binding.btnHome.visibility = View.INVISIBLE
+        binding.btnFavorites.visibility = View.INVISIBLE
+        binding.btnSettings.visibility = View.INVISIBLE
+    }
+
+    fun einblenden() {
+        binding.tvHeader.visibility = View.VISIBLE
+        binding.ivLuckyvLogo.alpha = 1F
+
+        binding.clNavBar.visibility = View.VISIBLE
+        binding.btnHome.visibility = View.VISIBLE
+        binding.btnFavorites.visibility = View.VISIBLE
+        binding.btnSettings.visibility = View.VISIBLE
     }
 }
