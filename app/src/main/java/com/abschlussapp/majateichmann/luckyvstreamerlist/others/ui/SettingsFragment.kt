48f3c51063a0fdf25bc2b5931ed55426c943e8bc
MajaTeichmann <katharina.nickname@hotmail.de>
2023-06-05 10:58:56 +0200

    05.06.2023 -> 10:39 Uhr
    --> setOnClickListener für deutsch/english button erstellen
    
    aktuelle fehler:
    
    -> beim mode wechsel ist topappbar ausgeblendet, bis ich zum homefragment navigiere
    
    -> favoriten- und einstellungen header haben verschiedene größen, obwohl beides textSize="24sp" & textStyle="bold" hat

--- app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/others/ui/SettingsFragment.kt
+++ app/src/main/java/com/abschlussapp/majateichmann/luckyvstreamerlist/others/ui/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.abschlussapp.majateichmann.luckyvstreamerlist.R
@@ -15,6 +16,16 @@ import com.abschlussapp.majateichmann.luckyvstreamerlist.R
 class SettingsFragment : Fragment() {
     private lateinit var btnLightMode: Button
     private lateinit var btnDarkMode: Button
+
+    private lateinit var btnGerman: Button
+    private lateinit var de_tvDescription1: TextView
+    private lateinit var de_tvDescription2: TextView
+    private lateinit var de_tvDescription3: TextView
+
+    private lateinit var btnEnglish: Button
+    private lateinit var en_tvDescription1: TextView
+    private lateinit var en_tvDescription2: TextView
+    private lateinit var en_tvDescription3: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
@@ -28,6 +39,14 @@ class SettingsFragment : Fragment() {
         }
         btnDarkMode.setOnClickListener {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        btnGerman.setOnClickListener {
+
+        }
+
+        btnEnglish.setOnClickListener {
+
         }
 
         return view
